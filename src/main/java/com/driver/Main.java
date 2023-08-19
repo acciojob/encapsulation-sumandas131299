2d49966868332afbc5d99b9dd7b8fc0ad56ad3de
Suman Das <sumandas131299@gmail.com>
2023-08-19 23:02:19 +0530

    Encapsulation solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //obj.name ="Suman";
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Suman-Das");
+        obj.getName();
+
+    }
 }
